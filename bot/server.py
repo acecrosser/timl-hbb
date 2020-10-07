259e25dfb4382c7d552d5bf5254b5af53fa3512a
acecrosser <acecrosser@yandex.ru>
2020-10-07 17:04:45 +0300

    Первая версия обработчика сообщения

--- bot/server.py
+++ bot/server.py
@@ -1,5 +1,7 @@
 from aiogram import Bot, Dispatcher, types, executor
 import receiver
+import exceptions
+
 
 API_TOKEN = '1328164745:AAFvR9tm2ivPlJsU1OGCpjjjdoOXct3GjaE'
 
@@ -12,27 +14,31 @@ async def send_welcome(msg: types.Message):
     await msg.answer('Тебя приветсвует Timl-bot!')
 
 
-@bd.message_handler(lambda msg: msg.text.startswith('Доход'))
-async def add_profit(msg: types.Message):
-    try:
-        data = receiver.add_profit(msg.text)
-    except receiver.NotCorrectMessage as e:
-        await msg.answer(str(e))
-        return
-    answer_profit = (
-        f'Додход от {data.name} в сумме {data.amount} добавлен в базу')
-    await msg.answer(answer_profit)
+# @bd.message_handler(lambda msg: msg.text.startswith('Доход'))
+# async def add_profit(msg: types.Message):
+#     try:
+#         data = receiver.add_profit(msg.text)
+#     except receiver.NotCorrectMessage as e:
+#         await msg.answer(str(e))
+#         return
+#     answer_profit = (
+#         f'Додход от {data.name} в сумме {data.amount} добавлен в базу')
+#     await msg.answer(answer_profit)
 
 
 @bd.message_handler()
 async def add_data(msg: types.Message):
     try:
-        data = receiver.add_data(msg.text)
-    except receiver.NotCorrectMessage as e:
+        data = receiver.general_data(msg.text)
+    except exceptions.NotCorrectMessage as e:
         await msg.answer(str(e))
         return
     answer_msg = (
-        f'Добавили в общую базу {data.amount} р., - {data.name}')
+        f'В базу внесена сумма - {data.amount} р.\n'
+        f'Тип: {data.name} \n'
+        f'Категория: {data.category} \n'
+        f'Источник: {data.name}\n'
+        f'Автор: {data.author}')
     await msg.answer(answer_msg)
 
 
