f903464e5dac3ddabfad39e34b4bc3646254a4c1
acecrosser <acecrosser@yandex.ru>
2020-11-16 17:24:41 +0300

    Подключил pgsql

--- bot_v02/data/dbase/models.py
+++ bot_v02/data/dbase/models.py
@@ -1,10 +1,17 @@
 import logging
 import sqlite3
+from datetime import datetime
+
+import psycopg2 as sql
 from typing import Dict
 
 sqlite3.OperationalError()
 
-connect = sqlite3.connect('base.db')
+# connect = sqlite3.connect('base.db')
+connect = sql.connect(dbname='db_finbot',
+                      user='bot_admin',
+                      password='PmeT3mn7tPynhVqY254M',
+                      host='localhost', port='5432')
 cursor = connect.cursor()
 
 
@@ -19,9 +26,9 @@ def make_default_db():
 
 def insert_data(table: str, column_items: Dict):
     columns = ', '.join(column_items.keys())
-    values = [tuple(column_items.values())]
-    number = ', '.join('?' * len(column_items.keys()))
-    cursor.executemany(
+    values = tuple(column_items.values())
+    number = ', '.join(['%s'] * len(column_items.keys()))
+    cursor.execute(
         f'INSERT INTO {table} '
         f'({columns})'
         f'VALUES ({number})', values)
@@ -29,6 +36,16 @@ def insert_data(table: str, column_items: Dict):
     # connect.close()
     logging.info('Данные успешно внесены...')
 
+
+# value_group = {
+#         'id_user': 123,
+#         'amount': 45,
+#         'grouping': 'ежеднев',
+#         'title': 'проезд',
+#         'time': 2020,
+# }
+#
+# insert_data('expense', value_group)
 
 if __name__ == '__main__':
     make_default_db()
