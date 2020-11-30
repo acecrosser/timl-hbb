2c2631cdc860f66886944c860594dbf77774a263
acecrosser <acecrosser@yandex.ru>
2020-11-30 20:53:28 +0300

    Новый отчет по расходам

--- bot_v02/handlers/users/orders.py
+++ bot_v02/handlers/users/orders.py
@@ -36,4 +36,4 @@ async def make_order(msg: types.Message):
     for i in work_data:
         sum = sum_title('expense', id_user=id_user, period=month, title=i[0])
         end_list += f'{i[0]} --- {sum[0]} руб.\n'
-    await msg.answer(f'Отчет по расходам за текущий месяц: \n\n {end_list}')
+    await msg.answer(f'Отчет по расходам за текущий месяц: \n\n{end_list}')
