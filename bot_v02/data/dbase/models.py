8167d8b5c698f5f1acafa5828c8175dee35f02f8
acecrosser <acecrosser@yandex.ru>
2020-11-30 18:14:05 +0300

    Новый отчет по расходам

--- bot_v02/data/dbase/models.py
+++ bot_v02/data/dbase/models.py
@@ -35,7 +35,10 @@ def insert_data(table: str, column_items: Dict):
 
 def today(table: str, id_user: str, period: str):
     cursor.execute(
-        f"SELECT sum(amount) FROM {table} WHERE id_user='{id_user}' AND time LIKE '{period}%'"
+        f"SELECT sum(amount) "
+        f"FROM {table} "
+        f"WHERE id_user='{id_user}' "
+        f"AND time LIKE '{period}%'"
     )
     summa_today = cursor.fetchone()
     return summa_today
@@ -43,7 +46,10 @@ def today(table: str, id_user: str, period: str):
 
 def distinct(table: str, id_user: str, period: str):
     cursor.execute(
-        f"SELECT DISTINCT title FROM {table} WHERE id_user='{id_user}' AND time LIKE '{period}%'"
+        f"SELECT DISTINCT title "
+        f"FROM {table}"
+        f" WHERE id_user='{id_user}' "
+        f"AND time LIKE '{period}%'"
     )
     distinct_data = cursor.fetchall()
     return distinct_data
@@ -51,7 +57,8 @@ def distinct(table: str, id_user: str, period: str):
 
 def sum_title(table: str, id_user: str, period: str, title: str) -> float:
     cursor.execute(
-        f"SELECT SUM(amount) FROM {table} "
+        f"SELECT SUM(amount) "
+        f"FROM {table} "
         f"WHERE id_user='{id_user}' "
         f"AND title LIKE '{title}%' "
         f"AND time LIKE '{period}%'"
