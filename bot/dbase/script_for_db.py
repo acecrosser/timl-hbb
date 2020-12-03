792ce3057699fc6a43193aba3c0afb5ebfe9cf3b
acecrosser <acecrosser@yandex.ru>
2020-12-03 17:45:06 +0300

    Удалил не нужное

--- bot/dbase/script_for_db.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-connect = psycopg2.connect(dbname='timl_money',
-                           user='bot_v02',
-                           password='OSXC715MICHElle',
-                           host='localhost')
-
-cursor = connect.cursor()
--- bot/exceptions.py
+++
@@ -1,4 +0,0 @@
-
-class NotCorrectMessage(Exception):
-    pass
-
